bbd82e9875ff97fee88f7bea0a9900fc30282fac
gaurav parajuli <gparajuli.1997@gmail.com>
2022-12-04 10:21:34 +0545

    user schema created

---
+++ models/user.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose'
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdminUser: {
+        type: Boolean,
+        default: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    resetToken: {
+        type: String,
+    },
+    resetTokenValidTill: {
+        type: Date,
+    },
+})
+
+export default model('User', UserSchema)
