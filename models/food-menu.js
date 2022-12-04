263288483d58380e4aa1f6fce5d44d736255ae9b
gaurav parajuli <gparajuli.1997@gmail.com>
2022-12-04 10:55:15 +0545

    food menu schema finalized

---
+++ models/food-menu.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+
+const FoodMenuSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        foodUnit: {
+            type: String,
+            required: true,
+        },
+        unitPrice: {
+            type: Number,
+            required: true,
+        },
+        foodCategory: {
+            type: Schema.Types.ObjectId,
+        },
+        organizationId: {
+            type: Schema.Types.ObjectId,
+        },
+    },
+    { timestamps: true }
+)
+
+export default model('FoodMenu', FoodMenuSchema)
